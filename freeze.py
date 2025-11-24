e68e19695b31252a171577d67071cd1c527e6110
marcelofcn <33524226+marcelofcn@users.noreply.github.com>
2025-11-24 17:51:36 -0300

    Refactor imports and update roteiro_detalhe generator
    Updated import statements and modified the generator function to use string keys.

--- freeze.py
+++ freeze.py
@@ -1,13 +1,14 @@
 from flask_frozen import Freezer
-from main import app, ROTEIROS_DB
+from main import app
+from data_roteiros import ROTEIROS_DB
 
 freezer = Freezer(app)
 
-# Gerar páginas individuais de cada roteiro
+# Rotas dinâmicas
 @freezer.register_generator
 def roteiro_detalhe():
-    for id, r in ROTEIROS_DB.items():
-        yield {"roteiro_id": int(id)}
+    for id_str, r in ROTEIROS_DB.items():
+        yield {"roteiro_id": int(id_str)}
 
 if __name__ == "__main__":
     freezer.freeze()
