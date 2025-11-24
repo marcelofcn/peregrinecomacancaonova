cbcebdd4c4edf74d909a6c039db65471d74b1e4c
marcelofcn <33524226+marcelofcn@users.noreply.github.com>
2025-11-24 15:44:26 -0300

    Refactor roteiro_detalhe to roteiro for dynamic routes

--- freeze.py
+++ freeze.py
@@ -1,16 +1,14 @@
-# freeze.py
 from flask_frozen import Freezer
 from main import app
 from data_roteiros import ROTEIROS_DB
 
-app.config['FREEZER_DESTINATION'] = 'build'
 freezer = Freezer(app)
 
+# Rotas dinâmicas de roteiros
 @freezer.register_generator
-def roteiro_detalhe():
-    for r in ROTEIROS_DB.values():
-        if r.get('empresa') == 'cancaonova':
-            yield {'roteiro_id': r['id']}
+def roteiro():
+    for r in ROTEIROS_DB:
+        yield {"id": r["id"]}
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     freezer.freeze()
