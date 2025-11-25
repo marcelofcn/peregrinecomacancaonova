d94041216c9dbc79b3833abc1f369428cc230f90
marcelofcn <33524226+marcelofcn@users.noreply.github.com>
2025-11-25 08:06:25 -0300

    Update freeze.py

--- freeze.py
+++ freeze.py
@@ -6,8 +6,9 @@ freezer = Freezer(app)
 
 @freezer.register_generator
 def roteiro_detalhe():
-    for id_str, r in ROTEIROS_DB.items():
-        yield {"roteiro_id": int(id_str)}
+    for r in obter_roteiros_do_banco():
+        yield {'id': r.id}
+
 
 if __name__ == "__main__":
     freezer.freeze()
