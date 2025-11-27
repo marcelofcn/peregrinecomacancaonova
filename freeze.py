d58cc0d07ac41321590eebf1d50199cef05f8b3c
marcelofcn <33524226+marcelofcn@users.noreply.github.com>
2025-11-27 09:48:13 -0300

    Update freeze.py

--- freeze.py
+++ freeze.py
@@ -1,30 +1,22 @@
-# freeze.py
-
 from flask_frozen import Freezer
 from data import ROTEIROS_DB
 from main import app
 
-# Apenas destino — NÃO repetir BASE_URL aqui
+app.config['FREEZER_BASE_URL'] = 'https://marcelofcn.github.io/peregrinecomacancaonova/'
 app.config['FREEZER_DESTINATION'] = 'docs'
-
-# Necessário para GitHub Pages em subdiretório
-app.config['FREEZER_SCRIPT_NAME'] = '/peregrinecomacancaonova'
 
 freezer = Freezer(app)
 
-
 @freezer.register_generator
 def roteiro_detalhe():
-    for r in ROTEIROS_DB:
-        yield 'roteiro_detalhe', {'id': r["id"]}
-
+    for id_str, r in ROTEIROS_DB.items():
+        yield 'roteiro_detalhe', {'id': int(id_str)}
 
 @freezer.register_generator
 def sobre():
     yield 'sobre'
 
-
 if __name__ == "__main__":
     print("Iniciando o processo de congelamento (freezing)...")
     freezer.freeze()
-    print("✅ Congelamento concluído na pasta 'docs'!")
+    print("✅ Congelamento concluído!")
